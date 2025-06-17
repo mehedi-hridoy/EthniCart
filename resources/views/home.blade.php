4822da798f1155bff62a120d3f79fea50714dab4
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-06-17 23:53:41 +0600

    Tailwind CDN Added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EthniCart</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <!-- tailwind cdn link -->
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> 
 </head>
 <body>
     <h1>EthniCart- From Earth to You</h1>
