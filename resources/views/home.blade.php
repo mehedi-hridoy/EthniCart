effb0beaaf07db92b43711fddef130c69a3249fe
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-06-17 23:40:13 +0600

    Favicon Added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EthniCart</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 </head>
 <body>
     <h1>EthniCart- From Earth to You</h1>
