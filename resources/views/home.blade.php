2321dc36a48430f684b9af25f5103fa1c807745d
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-06-20 22:33:42 +0600

    removed a content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     
 
-  // css need to be added 
+  <!-- // css need to be added  -->
 
 
 
