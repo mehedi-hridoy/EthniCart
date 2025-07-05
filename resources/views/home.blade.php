76b2306ca55aa635851aac930e860cd03fe4267b
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-07-05 21:33:14 +0600

    Updating JS Link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -129,29 +129,41 @@
         </div>
     </nav>
 
-
-
-
-
-
-
 </div>
 
-    
 
-    <script>
-        // some scripts here 
-        document.addEventListener('DOMContentLoaded', function() {
-            const mobileSearchToggle = document.querySelector('.md\\:hidden .fa-magnifying-glass').parentElement;
-            const mobileSearchBar = document.querySelector('.md\\:hidden.bg-white.border-t');
-            
-            let isSearchVisible = true;
-            
-            mobileSearchToggle.addEventListener('click', function() {
-                isSearchVisible = !isSearchVisible;
-                mobileSearchBar.style.display = isSearchVisible ? 'block' : 'none';
-            });
-        });
-    </script>
+
+ 
+          
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+
 </body>
 </html>
