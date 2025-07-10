10bf48697a5b1c964bc1dc9e844a42c0fe83ae29
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-07-11 01:33:05 +0600

    Commiting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -142,7 +142,7 @@
                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                         </svg>
-                        <span>SHOP BY CATEGORY</span>
+                        <span>SHOP BY CATEGORY for now</span>
                     </button>
                     
                     <!-- Dropdown Menu -->
