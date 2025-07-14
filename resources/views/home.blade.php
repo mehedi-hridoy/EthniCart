5b51178ee8458b14af42b44228021731c1dd559c
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-07-14 15:10:00 +0600

    Update beauty products image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -425,7 +425,7 @@
             <div class="group">
                 <a href="#" class="block bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
                     <div class="aspect-square relative">
-                        <img src="https://via.placeholder.com/300x300/D1FAE5/065F46?text=Fruits+%26+Vegetables" 
+                        <img src="images/beauty_items.png" 
                              alt="Fruits & Vegetables" 
                              class="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300">
                         <div class="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-10 transition-all duration-300"></div>
