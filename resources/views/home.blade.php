9e5c23accdf32fcb47292b179f9757465db03d47
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-07-14 21:17:18 +0600

    Commiting My work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,9 +32,6 @@
             }
         }
     </script>
-    <style>
-       
-    </style>
 </head>
 
 <body >
@@ -148,7 +145,7 @@
                     <!-- Dropdown Menu -->
                     <div id="categoryDropdown" class="absolute left-0 mt-2 w-64 bg-white rounded-md shadow-lg z-50 border border-gray-200 hidden">
                         <div class="py-2">
-                            <a href="/food" class="flex items-center px-4 py-3 text-sm text-gray-700 hover:bg-gray-50 hover:text-gray-900 transition-colors duration-150">
+                            <a href="{{ url('views/food.blade.php') }}" class="flex items-center px-4 py-3 text-sm text-gray-700 hover:bg-gray-50 hover:text-gray-900 transition-colors duration-150">
                                 <svg class="w-5 h-5 mr-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7"></path>
                                 </svg>
@@ -604,7 +601,7 @@
 
 
 
-
+<!-- Cart Section -->
 
 
 
