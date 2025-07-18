9ed10cd0ddbd2769d60be1a3e57f3a83b78792d1
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-07-18 23:06:15 +0600

    updated image of slide 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -319,18 +319,10 @@
             {{-- Image Slide 2 --}}
             <div class="min-w-full relative">
                 <a href="#" class="block w-full h-full">
-                    <img src="{{ asset('images/header2.jpg') }}"
+                    <img src="{{ asset('images/slide_2.png') }}"
                          alt="Slide 2" 
                          class="w-full h-full object-cover">
-                    <div class="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center">
-                        <div class="text-center text-white">
-                            <h2 class="text-3xl md:text-5xl font-bold mb-4">Fashion Collection</h2>
-                            <p class="text-lg md:text-xl mb-6">Explore the latest trends</p>
-                            <button class="bg-white text-gray-800 px-6 py-3 rounded-lg font-semibold hover:bg-gray-100 transition-colors">
-                                Explore Now
-                            </button>
-                        </div>
-                    </div>
+
                 </a>
             </div>
 
