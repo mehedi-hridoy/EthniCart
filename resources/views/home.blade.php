e82991fff5c06f089ad14bd14b067ded618a07ec
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-07-19 01:35:42 +0600

    Upadte About Us header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -277,6 +277,7 @@
                     <a href="#" class="text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium">Ethni Promise</a>
                     <a href="#" class="text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium">Stories</a>
                     <a href="#" class="text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium">Join as Seller</a>
+                    <a href="{{url('/aboutUs')}}"    class="text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium">About Us</a>
                 </div>
             </div>
         </div>
