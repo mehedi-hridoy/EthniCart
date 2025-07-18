60695299a75fa838adf7dba98600edfb1f11230a
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-07-18 22:35:55 +0600

    EthniPromise Setup

---
+++ resources/views/ethniPromise.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'EthniCart | Ethni Promise')
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
+@section('content')
+  
+@endsection
