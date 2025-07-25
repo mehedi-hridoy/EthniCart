d1192f8761758be8a56c9a98161057d12de589aa
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-07-25 22:28:29 +0600

    cleaing products echo friendly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -569,7 +569,7 @@
             <div class="group">
                 <a href="{{url('/gift')}}" class="block bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
                     <div class="aspect-square relative">
-                        <img  src="{{ asset('images/gifts_flowers.jpg') }}"
+                        <img  src="{{ asset('images/flowerNgifts.jpg') }}"
                              alt="Health & Wellness" 
                              class="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300">
                         <div class="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-10 transition-all duration-300"></div>
