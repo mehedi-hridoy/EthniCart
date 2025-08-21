2bce5d020f7b6bd5e90c354b3fd2e8c38a811191
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-08-21 13:43:03 +0600

    meat section update

--- resources/views/C2_Fish&Meat_Meat.blade.php
+++ resources/views/C2_Fish&Meat_Meat.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'EthniCart | Meat')
+@section('title', 'EthniCart |Meat')
 <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 @section('content')
 
@@ -10,11 +10,9 @@
     <div class="container mx-auto px-4 py-8">
         <div class="text-center mb-8">
             <h1 class="text-3xl md:text-4xl font-bold text-gray-900 mb-3">
-                Ethni<span class="text-[#6A9793]">Cart</span> Collection
+              Fresh <span class="text-green-600"> Meat</span> Items
             </h1>
-            <p class="text-base text-gray-600 max-w-2xl mx-auto">
-                Discover our curated selection of beautiful handcrafted furniture and decor
-            </p>
+            
         </div>
 
        
@@ -88,24 +86,13 @@
             <div class="flex items-center gap-1 mb-4">
                 @for($i = 1; $i <= 5; $i++)
                 <svg class="w-4 h-4 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M9.04@extends('layouts.app')
-
-@section('title', 'EthniCart | Sweets')
-<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-@section('content')
-
-
-
-
-  
-@endsection
-9 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
                 </svg>
                 @endfor
                 <span class="text-xs text-gray-500 ml-1">(4.8)</span>
             </div>
 
-  <!-- Price & Add to Cart -->
+             <!-- Price & Add to Cart -->
 <div class="flex items-center justify-between">
     <span class="text-xl font-bold text-gray-900">
         ৳{{ number_format($product->price, 2) }} 
@@ -125,6 +112,8 @@
         {{ (isset($product->stock) && $product->stock == 0) ? 'Sold Out' : 'Add to cart' }}
     </button>
 </form>
+
+
 </div>
 
 
@@ -165,6 +154,7 @@
 
   
 @endsection
+
 
 
 
